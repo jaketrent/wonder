37d19a36b6ada58b84ca06373b8f683cc26c16b5
jaketrent <trent.jake@gmail.com>
2019-07-10 07:05:35 -0600

    refactor(client): style header

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -8,17 +8,38 @@
 </script>
 
 <style>
-	h1 {
-		color: orange;
-	}
+  h1 {
+    color: red;
+    margin: 0 0 1rem 0;
+    font-size: 1.25rem;
+  }
+  nav :global(a) {
+    padding: 0.25rem 1.25rem;
+    border: 1px solid red;
+    border-radius: 2px;
+    text-decoration: none;
+    color: black;
+    font-size: 0.85rem;
+    text-transform: uppercase;
+  }
+  nav :global(a):hover,
+  nav :global(a):focus {
+    background: red;
+    color: #fff;
+  }
 </style>
 
-<h1>Dad's Independent Summer of Wonder</h1>
-
 <Router url="{url}">
-  <nav>
-    <Link to="/">Home</Link>
-  </nav>
+  <header>
+    <h1>Dad's Independent Summer of Wonder</h1>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
   <div>
     <Route path="/"><WonderCreate /></Route>
     <Route path="/users/:userId" component="{UserDetail}" />
