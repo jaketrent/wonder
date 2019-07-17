91959c2b14bc566f9def231967b54f84c7fe1784
jaketrent <trent.jake@gmail.com>
2019-07-17 06:46:06 -0600

    style user list at page bottom

--- client/src/UserList.svelte
+++ client/src/UserList.svelte
@@ -3,8 +3,34 @@
   export let users
   export let counts
 </script>
+
 <style>
+  ul {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    background: #9E2F96;
+    margin: 0;
+    padding: 12px 0;
+  }
+  button {
+    color: #FFBF00;
+    border: 1px solid #FFBF00;
+    background: none;
+    font-weight: 600;
+  }
+  button:hover,
+  button:focus,
+  button:active {
+    background: #FFBF00;
+    color: #9E2F96;
+    outline: none;
+  }
 </style>
+
 <ul>
   {#each users as user}
     <li>
